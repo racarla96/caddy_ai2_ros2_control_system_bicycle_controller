b00c72eed4cbaa70d10c44a5bff40fcf2e684b3f
Rafael Carbonell Lázaro <racarla96@gmail.com>
2025-12-17 21:12:27 +0100

    Update full_teleop.launch.py

--- bringup/launch/full_teleop.launch.py
+++ bringup/launch/full_teleop.launch.py
@@ -5,6 +5,7 @@ from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import Command
 from launch.substitutions import FindExecutable
+from launch_ros.actions import Node
 from caddy_ai2_ros2_common.launch_utils import read_update_rate_from_controller_yaml
 
 def generate_launch_description():
